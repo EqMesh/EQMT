c391e3956b03bc5a2f4e6869c4d817c7f6fd1e7a
nullx8 <mario@hennenberger.de>
2025-12-06 19:28:32 +0700

    slimmed down (3 includes)

--- contracts/EQMT.sol
+++ contracts/EQMT.sol
@@ -2,9 +2,6 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts@4.7.0/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts@4.7.0/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts@4.7.0/access/Ownable.sol";
-import "@openzeppelin/contracts@4.7.0/utils/Counters.sol";
 import "@openzeppelin/contracts@4.7.0/utils/Strings.sol";
 import "@openzeppelin/contracts@4.7.0/utils/Base64.sol";
 
